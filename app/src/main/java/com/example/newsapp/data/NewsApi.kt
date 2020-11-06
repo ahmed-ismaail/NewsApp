d1e2266051c84a8180dbca7d84d5d3ae08801567
ahmed.elmohame1997.ae@gmail.com <ahmed.elmohame1997.ae@gmail.com>
2020-11-06 13:44:10 +0200

    retrofit setup

---
+++ app/src/main/java/com/example/newsapp/data/NewsApi.kt
@@ -0,0 +1,29 @@
+package com.example.newsapp.data
+
+import com.example.newsapp.util.Constants
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+//http://newsapi.org/v2/everything?q=bitcoin&from=2020-10-06&sortBy=publishedAt&apiKey=80c2e0be242c4772a3fb66327464e527
+interface NewsApi {
+    @GET("v2/top_headlines")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode: String = "us",
+        @Query("page")
+        pagenumber : Int = 1,//to make the request get only the first page (first 20 articles)
+        @Query("apiKey")
+        apiKey: String = Constants.API_KEY
+    ): Response<NewsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("country")
+        searchQuery: String,
+        @Query("page")
+        pagenumber : Int = 1,//to make the request get only the first page (first 20 articles)
+        @Query("apiKey")
+        apiKey: String = Constants.API_KEY
+    ): Response<NewsResponse>
+}
---
+++ app/src/main/java/com/example/newsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "80c2e0be242c4772a3fb66327464e527"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
